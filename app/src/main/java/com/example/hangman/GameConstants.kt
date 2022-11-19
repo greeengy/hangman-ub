a8f0bbaf51fbf9a50d4bb6f4bfeb7252545ea4d7
Gunay <gunay.magerramova11@yandex.ru>
2022-11-19 23:25:15 +0300

    i hope

---
+++ app/src/main/java/com/example/hangman/GameConstants.kt
@@ -0,0 +1,12 @@
+package com.example.hangman
+
+object GameConstants {
+    val words = listOf(
+    "Электротехника",
+    "Схемотехника",
+        "Физика",
+        "Когеренция",
+        "Триггеры",
+        "Мультифлексор"
+    )
+}
