caf27fe57a193fb0a6139ddb14ee2886001316e2
Gunay <gunay.magerramova11@yandex.ru>
2022-11-22 20:10:59 +0300

    server_requests

--- src/main/java/com/greeengy/servhang/controllers/WordsController.java
+++ src/main/java/com/greeengy/servhang/controllers/WordsController.java
@@ -1,2 +1,26 @@
-package com.greeengy.servhang.controllers;public class WordsController {
+package com.greeengy.servhang.controllers;
+
+import com.greeengy.servhang.model.words.Words;
+import com.greeengy.servhang.model.words.WordsDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class WordsController {
+    @Autowired
+    private WordsDao wordsDao;
+
+    @GetMapping("/words/get-all")
+    public List<Words> getAllWords(){
+        return wordsDao.getAllWords();
+    }
+
+    @PostMapping("/words/save")
+    public Words save(@RequestBody Words words){
+        return wordsDao.save(words);
+    }
 }
