fe4e70bcd5693491a600269a386f5a72f54104fc
Gunay <gunay.magerramova11@yandex.ru>
2022-11-22 18:02:27 +0300

    testing_the_database_connection

--- src/test/java/com/greeengy/servhang/ServhangApplicationTests.java
+++ src/test/java/com/greeengy/servhang/ServhangApplicationTests.java
@@ -1,13 +1,2 @@
-package com.greeengy.servhang;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ServhangApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+package com.greeengy.servhang;public class ServhangApplicationTests {
 }
